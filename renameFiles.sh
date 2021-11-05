7f3b5539b132bfbc93112ab35cbef00123aecf54
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2021-11-05 08:26:02 -0300

    Update renameFiles.sh
    english version

--- renameFiles.sh
+++ renameFiles.sh
@@ -5,6 +5,10 @@
 #cada arquivo desse diretório, dando ao arquivo o nome correspondente à primeira 
 #palavra encontrada no conteúdo do próprio arquivo.
 
+# EN: Consider a certain directory that has several text files. These files have various names, 
+#not obeying any criteria. Write a script that renames each file in that directory, giving the 
+#file the name corresponding to the first word found in the contents of the file itself.
+
 for nome in ./arquivos/*.txt
 do
   novoNome=$(echo $(cat $nome | cut -d ' ' -f1))
