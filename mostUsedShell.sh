584e25d763a875994bc46e4ff5f1868365c58a0a
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2021-11-05 08:21:34 -0300

    Update mostUsedShell.sh
    english version

--- mostUsedShell.sh
+++ mostUsedShell.sh
@@ -3,7 +3,11 @@
 # Uma das informações armazenadas no arquivo /etc/passwd é o shell utilizado por cada usuário do sistema (campo 7). Desenvolva um script que informe qual é o shell mais utilizado.
 #Exemplo: /bin/sh => 17 usuários.
 
+# EN: One of the information stored in the /etc/passwd file is the shell used by each user on the system (field 7). Develop a script that informs which is the most used shell.
+# Example: /bin/sh => 17 users.
+
 #recebe todos os shells em uso
+# EN: get all shells in use
 shelluso=$( cat /etc/passwd | cut -d : -f7)
 shellvalid=$( cat /etc/shells | cut -d '#' -f1)
 
